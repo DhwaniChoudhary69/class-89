e4bce7e2e9ea17e33a1dd5f663f308823e50fbff
DhwaniChoudhary69 <82637273+DhwaniChoudhary69@users.noreply.github.com>
2021-04-16 11:59:50 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+function adduser(){
+player1=document.getElementById("Player1_name_input").value;
+player2=document.getElementById("Player2_name_input").value;
+localStorage.setItem("player1",player1);
+localStorage.setItem("player2",player2);
+}
